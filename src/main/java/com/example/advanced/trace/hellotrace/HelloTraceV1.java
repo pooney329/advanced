926679465a902373e9ed9754185d862067f7b3f7
janghyeonwoo <blue672@naver.com>
2024-12-28 00:24:19 +0900

    로그 추적기 추가 테스트 코드 추가

--- src/main/java/com/example/advanced/trace/hellotrace/HelloTraceV1.java
+++ src/main/java/com/example/advanced/trace/hellotrace/HelloTraceV1.java
@@ -54,7 +54,7 @@ public class HelloTraceV1 {
         if (e == null) {
             log.info("[{}] {}{} time={}ms", traceId.getId(), addSpace(COMPLETE_PREFIX, traceId.getLevel()), status.getMessage(), resultTimeMs);
         } else {
-            log.info("[{}] {}{} time={}ms ex={}", traceId.getId(), addSpace(EX_PREFIX, traceId.getLevel()), status.getMessage(), resultTimeMs, e.getMessage());
+            log.info("[{}] {}{} time={}ms ex={}", traceId.getId(), addSpace(EX_PREFIX, traceId.getLevel()), status.getMessage(), resultTimeMs, e.toString());
         }
     }
 
