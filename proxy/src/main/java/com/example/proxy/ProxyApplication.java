2bdeffd57814aec018f5590165b20f1fe8b2b10e
janghyeonwoo <blue672@naver.com>
2024-12-30 00:35:10 +0900

    Proxy모듈 -> 구현체로 직접 빈 수동 생성

--- proxy/src/main/java/com/example/proxy/ProxyApplication.java
+++ proxy/src/main/java/com/example/proxy/ProxyApplication.java
@@ -1,6 +1,7 @@
 package com.example.proxy;
 
 import com.example.proxy.config.AppV1Config;
+import com.example.proxy.config.AppV2Config;
 import org.springframework.boot.SpringApplication;
 import org.springframework.boot.autoconfigure.SpringBootApplication;
 import org.springframework.context.annotation.Import;
@@ -11,8 +12,8 @@ import org.springframework.context.annotation.Import;
  * {@link AppV1Config} 와 같은 Config를 내가 원하는 버전을 선택해서 Bean으로 등록하지 못하기 때문
  */
 
-@Import(AppV1Config.class) //버전 별로 Config를 다르게 하기 위해 Config 별도 지정하여 Bean으로 등록
-@SpringBootApplication(scanBasePackages = "com.example.proxy.app")
+@Import(AppV2Config.class) //버전 별로 Config를 다르게 하기 위해 Config 별도 지정하여 Bean으로 등록
+@SpringBootApplication(scanBasePackages = "com.example.proxy.app.v3")
 public class ProxyApplication {
 
     public static void main(String[] args) {
