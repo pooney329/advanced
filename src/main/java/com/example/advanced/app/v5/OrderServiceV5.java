2628a2293e7fa0413bdce4bdd483c18cb0b123a2
janghyeonwoo <blue672@naver.com>
2024-12-29 21:28:06 +0900

    템플릿 콜백 패턴 application 적용

---
+++ src/main/java/com/example/advanced/app/v5/OrderServiceV5.java
@@ -0,0 +1,23 @@
+package com.example.advanced.app.v5;
+
+import com.example.advanced.trace.callback.TraceTemplate;
+import com.example.advanced.trace.logtrace.LogTrace;
+import com.example.advanced.trace.template.AbstractTemplate;
+import lombok.RequiredArgsConstructor;
+import org.springframework.stereotype.Service;
+
+@Service
+public class OrderServiceV5 {
+
+    private final OrderRepositoryV5 orderRepository;
+    private final TraceTemplate traceTemplate;
+
+    public OrderServiceV5(OrderRepositoryV5 orderRepository, LogTrace trace) {
+        this.orderRepository = orderRepository;
+        this.traceTemplate = new TraceTemplate(trace);
+    }
+
+    public void orderItem(String itemId) {
+        traceTemplate.execute("OrderService.orderItem()", () -> {orderRepository.save(itemId); return null;});
+    }
+}
