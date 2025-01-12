39a9bfffb6dac3f77c5a2f220ac40869ad8b5920
janghyeonwoo <blue672@naver.com>
2025-01-12 21:38:51 +0900

    NameMatchMethodPointcut 사용한 포인트 컷

--- proxy/src/test/java/com/example/proxy/advisor/AdvisorTest.java
+++ proxy/src/test/java/com/example/proxy/advisor/AdvisorTest.java
@@ -12,6 +12,7 @@ import org.springframework.aop.MethodMatcher;
 import org.springframework.aop.Pointcut;
 import org.springframework.aop.framework.ProxyFactory;
 import org.springframework.aop.support.DefaultPointcutAdvisor;
+import org.springframework.aop.support.NameMatchMethodPointcut;
 
 import java.lang.reflect.Method;
 
@@ -20,16 +21,15 @@ public class AdvisorTest {
 
 
     /**
-     *  1-1
-     *   Advisor는 하나의 포인트 컷과 하나의 어드바이스로 구성된다.
-     *
+     * 1-1
+     * Advisor는 하나의 포인트 컷과 하나의 어드바이스로 구성된다.
+     * <p>
      * {@link DefaultPointcutAdvisor}는 'Advisor' 인터페이스의 가장 일반적인 구현체이다.
      * {@link ProxyFactory#addAdvisor(Advisor)}의 내부 로직을 보면 결국 {@link DefaultPointcutAdvisor} 라는 Advisor를 사용한다.
      * 결국 addAdvisor는 사용하기 쉽게 제공하는 편의 메소드이다.
-     *
      */
     @Test
-    void advisorTest1(){
+    void advisorTest1() {
         ServiceInterface target = new ServiceImpl();
         ProxyFactory proxyFactory = new ProxyFactory(target);
         DefaultPointcutAdvisor advisor = new DefaultPointcutAdvisor(Pointcut.TRUE, new TimeAdvice());
@@ -47,7 +47,7 @@ public class AdvisorTest {
 
     @Test
     @DisplayName("직접 만든 포인트 컷")
-    void advisorTest2(){
+    void advisorTest2() {
         ServiceInterface target = new ServiceImpl();
         ProxyFactory proxyFactory = new ProxyFactory(target);
         DefaultPointcutAdvisor advisor = new DefaultPointcutAdvisor(new MyPointcut(), new TimeAdvice());
@@ -59,8 +59,32 @@ public class AdvisorTest {
     }
 
     /**
-     *  직접 구현한 포인트 컷
-     *  항상 클래스 필터는 True를 반환
+     *  스프링이 제공하는 포인트 컷 중 하나인 이름으로 찾는 NameMatchMethodPointcut를 사용한다
+     *  - JdkRegexpMethodPointCut
+     *  - TruePointcut
+     *  - AnnotationMatchingPointcut
+     *  - AspectJexpressionPointcut
+     */
+    @Test
+    @DisplayName("스프링이 제공하는 포인트 컷")
+    void advisorTest3() {
+        ServiceInterface target = new ServiceImpl();
+        ProxyFactory proxyFactory = new ProxyFactory(target);
+        NameMatchMethodPointcut pointcut = new NameMatchMethodPointcut();
+        pointcut.setMappedName("save");
+
+        DefaultPointcutAdvisor advisor = new DefaultPointcutAdvisor(pointcut, new TimeAdvice());
+        proxyFactory.addAdvisor(advisor);
+        ServiceInterface proxy = (ServiceInterface) proxyFactory.getProxy();
+
+        proxy.save();
+        proxy.find();
+    }
+
+
+    /**
+     * 직접 구현한 포인트 컷
+     * 항상 클래스 필터는 True를 반환
      */
     static class MyPointcut implements Pointcut {
         @Override
@@ -91,6 +115,7 @@ public class AdvisorTest {
          * isRuntime()의 값에 따라 메소드 호출이 달라진다.
          * false : {@link MethodMatcher#matches(Method, Class)} 를 호출하게 되고 클래스의 정적 정보만 사용 하기 때문에 스프링이 내부에서 캐싱을 통한 성능 향상이 가능
          * true  : {@link MethodMatcher#matches(Method, Class, Object...)} 를 호출 하게 되고 매개변수가 동적으로 변경된다고 가정하기 때문에 캐싱을 하지 않는다.
+         *
          * @return
          */
         @Override
