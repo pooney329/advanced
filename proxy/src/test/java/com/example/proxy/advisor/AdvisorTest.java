159fe58526fb854799dec12397508e5a751d0a19
janghyeonwoo <blue672@naver.com>
2025-01-12 21:31:50 +0900

    직접 포인트 컷을 만든 Advisor

--- proxy/src/test/java/com/example/proxy/advisor/AdvisorTest.java
+++ proxy/src/test/java/com/example/proxy/advisor/AdvisorTest.java
@@ -3,12 +3,19 @@ package com.example.proxy.advisor;
 import com.example.proxy.common.advice.TimeAdvice;
 import com.example.proxy.common.service.ServiceImpl;
 import com.example.proxy.common.service.ServiceInterface;
+import lombok.extern.slf4j.Slf4j;
+import org.junit.jupiter.api.DisplayName;
 import org.junit.jupiter.api.Test;
 import org.springframework.aop.Advisor;
+import org.springframework.aop.ClassFilter;
+import org.springframework.aop.MethodMatcher;
 import org.springframework.aop.Pointcut;
 import org.springframework.aop.framework.ProxyFactory;
 import org.springframework.aop.support.DefaultPointcutAdvisor;
 
+import java.lang.reflect.Method;
+
+@Slf4j
 public class AdvisorTest {
 
 
@@ -31,5 +38,69 @@ public class AdvisorTest {
 
         proxy.save();
         proxy.find();
+    }
+
+
+    /**
+     * new DefaultPointcutAdvisor(Pointcut.TRUE, new TimeAdvice()); 과 같이 {@link Pointcut.TRUE} 를 사용하면 항상 대상이 된다.
+     */
+
+    @Test
+    @DisplayName("직접 만든 포인트 컷")
+    void advisorTest2(){
+        ServiceInterface target = new ServiceImpl();
+        ProxyFactory proxyFactory = new ProxyFactory(target);
+        DefaultPointcutAdvisor advisor = new DefaultPointcutAdvisor(new MyPointcut(), new TimeAdvice());
+        proxyFactory.addAdvisor(advisor);
+        ServiceInterface proxy = (ServiceInterface) proxyFactory.getProxy();
+
+        proxy.save();
+        proxy.find();
+    }
+
+    /**
+     *  직접 구현한 포인트 컷
+     *  항상 클래스 필터는 True를 반환
+     */
+    static class MyPointcut implements Pointcut {
+        @Override
+        public ClassFilter getClassFilter() {
+            return ClassFilter.TRUE;
+        }
+
+        @Override
+        public MethodMatcher getMethodMatcher() {
+            return new MyMethodMatcher();
+        }
+    }
+
+    static class MyMethodMatcher implements MethodMatcher {
+
+        private String matchName = "save";
+
+        @Override
+        public boolean matches(Method method, Class<?> targetClass) {
+
+            boolean result = method.getName().equals(matchName);
+            log.info("포인트 컷 호출 method={} targetClass ={}", method.getName(), targetClass);
+            log.info("포인트 컷 결과 result={}", result);
+            return result;
+        }
+
+        /**
+         * isRuntime()의 값에 따라 메소드 호출이 달라진다.
+         * false : {@link MethodMatcher#matches(Method, Class)} 를 호출하게 되고 클래스의 정적 정보만 사용 하기 때문에 스프링이 내부에서 캐싱을 통한 성능 향상이 가능
+         * true  : {@link MethodMatcher#matches(Method, Class, Object...)} 를 호출 하게 되고 매개변수가 동적으로 변경된다고 가정하기 때문에 캐싱을 하지 않는다.
+         * @return
+         */
+        @Override
+        public boolean isRuntime() {
+            return false;
+        }
+
+        @Override
+        public boolean matches(Method method, Class<?> targetClass, Object... args) {
+            return false;
+        }
     }
 }
