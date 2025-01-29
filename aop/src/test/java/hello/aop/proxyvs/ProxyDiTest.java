7887ea98fc56cb653472e9f67dc806059fbe6c85
janghyeonwoo <blue672@naver.com>
2025-01-29 18:23:52 +0900

    프록시 기술과 한계 - 의존관계 주입

---
+++ aop/src/test/java/hello/aop/proxyvs/ProxyDiTest.java
@@ -0,0 +1,35 @@
+package hello.aop.proxyvs;
+
+import hello.aop.member.MemberService;
+import hello.aop.member.MemberServiceImpl;
+import hello.aop.proxyvs.code.ProxyDiAspect;
+import lombok.extern.slf4j.Slf4j;
+import org.junit.jupiter.api.Test;
+import org.springframework.beans.factory.annotation.Autowired;
+import org.springframework.boot.test.context.SpringBootTest;
+import org.springframework.context.annotation.Import;
+
+/**
+ * JDK 동적 프록시는 대상 객체인 MemberServiceImpl 타입에 의존관계를 주입할 수 없다.
+ * CGLIB 프록시는 대상 객체인 MemberServiceImpl 타입에 의존관계 주입을 할 수 있다.
+ */
+@Slf4j
+@Import(ProxyDiAspect.class)
+//@SpringBootTest(properties = {"spring.aop.proxy-target-class=false"}) // jdk 동적 프록시
+@SpringBootTest(properties = {"spring.aop.proxy-target-class=true"}) // cjlib 동적 프록시
+public class ProxyDiTest {
+
+
+    @Autowired
+    MemberService memberService;
+
+    @Autowired
+    MemberServiceImpl memberServiceImpl;
+
+    @Test
+    void go(){
+        log.info("memberService class={}", memberService.getClass());
+        log.info("memberServiceImpl class={}", memberServiceImpl.getClass());
+        memberServiceImpl.hello("hello");
+    }
+}
